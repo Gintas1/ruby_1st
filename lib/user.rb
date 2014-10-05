dc514accf78fd8c88da9c96f663c2ddec369e9d2
Gintas <gintautas.kras@gmail.com>
2014-10-05 20:47:59 +0300

    added remove_from_cart_method

--- lib/user.rb
+++ lib/user.rb
@@ -26,4 +26,17 @@ class User
     @cart.price = 0
 	@cart.itemlist = []
   end
+  
+  def remove_from_cart(item)
+    if(item.is_a?(Game))
+      if(cart.itemlist.include? item)
+	    @cart.itemlist.delete(item)
+	    @cart.price -= item.price
+	  else
+	    raise StandartError, 'item you want to remove is not in a list'
+      end
+	else
+	  raise TypeError, 'item instance is not a type of a Game'
+	end
+  end
 end
