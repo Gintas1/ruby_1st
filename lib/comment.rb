3cf4d48bc32eac97766263d92ffdc809b220d6db
Gintas1 <gintautas.kras@gmail.com>
2014-10-30 20:59:38 +0200

    comment class added and comment_spec changed

--- lib/comment.rb
+++ lib/comment.rb
@@ -1,4 +1,9 @@
 #class for game comments
 class Comment 
-
+attr_accessor :time, :text, :user
+  def initialize(data)
+    @time = Time.now
+	@text = data[:text]
+	@user = data[:user]
+  end
 end
--- spec/lib/comment_spec.rb
+++ spec/lib/comment_spec.rb
@@ -5,16 +5,16 @@ require 'comment'
 describe Comment do
   describe '#initialize' do
     it 'checks text of a comment' do
-      comment = Comment.New(text: 'text test', user: User.new(username: 'test', password: 'test'), )
+      comment = Comment.new(text: 'text test', user: User.new(username: 'test', password: 'test'), )
       expect(comment.text).to eq('text test')
 	end
     it 'checks author of a comment' do
-      comment = Comment.New(text: 'text test', user: User.new(username: 'test', password: 'test'), )
-      expect(comment.user.name).to eq('test')
+      comment = Comment.new(text: 'text test', user: User.new(username: 'test', password: 'test'), )
+      expect(comment.user.username).to eq('test')
 	end
     it 'checks time of a comment' do
-      comment = Comment.New(text: 'text test', user: User.new(username: 'test', password: 'test'), )
-      expect(comment.time).to be(Time)
+      comment = Comment.new(text: 'text test', user: User.new(username: 'test', password: 'test'), )
+      expect(comment.time).to be_a(Time)
 	end
   end
 end
