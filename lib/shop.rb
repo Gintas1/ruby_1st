7a0a4b021d5c294314c8351e9f2e102e27eea719
Gintas1 <gintautas.kras@gmail.com>
2014-11-01 22:36:15 +0200

    user can now view games and add them to cart

--- lib/shop.rb
+++ lib/shop.rb
@@ -6,8 +6,11 @@ require_relative 'comment'
 require_relative 'purchase'
 require_relative 'cart'
 class Shop
-  attr_accessor :users, :games, :current_user, :name
+  attr_accessor :users, :games, :current_user, :name, :price_modifier,
+                :currency, :sales
+  
   def initialize
+    @sales = []
     @users = []
 	@games = []
 	@current_user = nil
@@ -19,13 +22,12 @@ class Shop
   def begin
     puts 'welcome to online store ' + @name
     loop do
+	  puts
       puts 'what would you like to do?'
       puts 'reg - register'
       puts 'login - to login'
       puts 'exit - to exit'
-      puts
-      action = gets
-      action.rstrip!
+      action = gets.downcase.rstrip!
       case action
         when 'reg'
           register
@@ -43,15 +45,13 @@ class Shop
   def register
     loop do
       puts 'do you want to go back? [y/n]'
-      if gets.rstrip! == 'y'
+      if gets.downcase.rstrip! == 'y'
         break
       end
       puts 'enter username:'
-      username = gets
-      username.rstrip!
+      username = gets.downcase.rstrip!
       puts 'enter password'
-      password = gets
-      password.rstrip!
+      password = gets.downcase.rstrip!
       if @users.any? { |user| user.check_username(username) }
         puts 'username is already in use'
         puts
@@ -67,28 +67,174 @@ class Shop
   def login
     loop do
       puts 'do you want to go back?[y/n]'
-      if gets.rstrip! == 'y'
+      if gets.downcase.rstrip! == 'y'
         break
       end
 	  puts 'enter username'
-	  username = gets.rstrip!
+	  username = gets.downcase.rstrip!
 	  puts 'enter password'
-	  password = gets.rstrip!
+	  password = gets.downcase.rstrip!
       @current_user = @users.find { |user| user.check_data(username, password) }
 	  puts @current_user
 	  if @current_user.is_a?(User) || @current_user.is_a?(Admin)
         puts 'you have loged in'
+		main_window
         break
       else
         puts 'Wrong username or password'
       end
     end
   end
+  
+  def main_window
+    puts 'welcome ' + @current_user.username
+	loop do
+	puts
+      puts 'what would you like to do?'
+	  puts 'logout - to logout'
+	  puts 'delete - to delete your account'
+	  puts 'user - to view your account'
+	  puts 'games - to view all games'
+	  puts 'currency - to change currency game prices are shown'
+	  action = gets.downcase.rstrip!
+	  case action
+	    when 'logout'
+          @current_user = nil
+		  break
+		when 'delete'
+		  @users.delete(@current_user)
+		  @current_user = nil
+		  break
+		when 'user'
+		  user_window
+		when 'games'
+		  gamelist_window
+		when 'currency'
+          change_currency
+		else
+		  puts 'wrong action'
+      end
+	end
+  end
+  
+  def change_currency
+    puts
+    puts 'select currency from avalible' + @price_modifier.keys.to_s
+	selection = gets.downcase.rstrip!
+	case selection
+	  when 'ltl'
+	    @currency = 'LTL'
+	  when 'eur'
+	    @currency = 'EUR'
+	  else
+	    puts 'wrong selection'
+	end
+	puts @currency
+  end
+
+  def user_window
+    
+  end
+
+  def gamelist_window
+    loop do
+	  puts
+	  puts 'the folowing is a list of games we have to offer(name price)'
+	  @games.each{ |game| puts game.name + ' ' + (game.price * @price_modifier[@currency])
+	              .round(2).to_s + @currency }
+	  puts 'what would you like to do?'
+	  puts 'inspect - to further inspect selected game'
+	  puts 'add - to add selected game to your cart'
+	  puts 'back - to return to main window'
+	  action = gets.downcase.rstrip!
+	  case action
+	    when 'inspect'
+		  puts 'type a name of a game you want to inspect'
+		  game_name = gets.downcase.rstrip!
+		  selected_game = @games.find { |game| game.name.downcase == game_name }
+		  if selected_game.is_a?(Game) 
+		    selected_game_window(selected_game)
+		  else
+		    puts 'game not found'
+		  end
+		when 'add'
+		  puts 'type a name of a game you want to inspect'
+		  game_name = gets.downcase.rstrip!
+		  selected_game = @games.find { |game| game.name.downcase == game_name }
+		  if selected_game.is_a?(Game) 
+		    puts 'how many would you like to add?(1..10)'
+		    num = gets.to_i
+		    if num.between?(1, 10)
+			  @current_user.add_to_cart(selected_game, num)
+			else
+			  @current_user.add_to_cart(selected_game)
+			end
+		  else
+		    puts 'game not found'
+		  end
+		when 'back'
+		  break
+	    else
+		  puts 'wrong selection'
+	  end
+	end
+  end
+  
+  def selected_game_window(game)
+    loop do
+	  puts
+      puts 'Game name: ' + game.name
+	  puts 'Game genre: ' + game.genre 
+	  puts 'Game description: ' + game.description
+	  puts 'Game price(' + @currency + '): ' + (game.price * 
+	                                            @price_modifier[@currency])
+	  										  .round(2).to_s
+	  puts 'comments:'
+	  game.comments.each { |x| puts x.id.to_s + ' ' + x.time.to_s + ' ' + x.user.username + ' ' + x.text }
+	  if game.ratings.empty?
+	    puts 'Game rating: no ratings yet'
+	  else
+	    puts 'Game rating: ' + (game.ratings.values.instance_eval { reduce(:+) / size.to_f}).to_s
+	  end
+	  puts
+	  puts 'what would you like to do?'
+	  puts 'add - to add this game to cart'
+	  puts 'back - to return to game list window'
+	  action = gets.downcase.rstrip!
+	  case action
+	    when 'add'
+		  puts 'how many would you like to add?(1..10)'
+		  num = gets.to_i
+		  if num.between?(1, 10)
+		    @current_user.add_to_cart(game, num)
+		  else
+		    @current_user.add_to_cart(game)
+		  end
+		when 'back'
+		  break
+		else
+		  puts 'wrong selection'
+      end
+	end
+  end
+
 end
-
 shop = Shop.new
-#shop.users.push(User.new(username: 'a', password: 'a'))
+game = Game.new(name: 'game1', genre: 'Genre test',
+                description: 'Game description test', price: 10)
+user = User.new(username: 'a', password: 'a')
+user2 = User.new(username: 'b', password: 'b')
+game.ratings[user] = 5
+game.ratings[user2] = 1
+game.comments.push(Comment.new(user: user, id: 2, text: 'labas'))
+game.comments.push(Comment.new(user: user, id: 3, text: 'vakaras'))
+shop.users.push(user)
+shop.users.push(user2)
+shop.games.push(game)
+shop.games.push(Game.new(name: 'game2', genre: 'Genre test',
+                      description: 'Game description test', price: 20))
+shop.games.push(Game.new(name: 'game3', genre: 'Genre test',
+                      description: 'Game description test', price: 30))
 shop.begin
-
 #admin = Admin.new(username: 'test', password: 'test')
 #admin.sort
