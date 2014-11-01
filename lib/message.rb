2ffe88ece3d588842b9aee1cb6212bdd2743919f
Gintas1 <gintautas.kras@gmail.com>
2014-11-01 15:35:57 +0200

    message class initialization added

--- lib/message.rb
+++ lib/message.rb
@@ -1,7 +1,12 @@
 #class for message 
 class Message
-  attr_accessor :read, :to, :from, :date, :topic, :text 
+  attr_accessor :read, :receiver, :sender, :date, :topic, :text 
   def initialize(data)
-    
+    @read = false
+	@receiver = data[:receiver]
+	@sender = data[:sender]
+	@date = Time.now
+	@topic = data[:topic]
+	@text = data[:text]
   end
 end
--- spec/lib/message_spec.rb
+++ spec/lib/message_spec.rb
@@ -3,47 +3,47 @@ require 'spec_helper'
 require 'message'
 
 describe Message do
-  def '#initialize' do
+  describe '#initialize' do
     it 'checks if new message is not read' do
       receiver = User.new(username: 'test', password: 'test')
       sender = User.new(username: 'test', password: 'test')
-      message = Message.new(topic: 'topic', text: 'text', from: sender,
-                          to: receiver)
-      expect(message.read).to be_a(false)
+      message = Message.new(topic: 'topic', text: 'text', sender: sender,
+                          receiver: receiver)
+      expect(message.read).to be false
     end
     it 'checks if new message has a topic named topic' do
       receiver = User.new(username: 'test', password: 'test')
       sender = User.new(username: 'test', password: 'test')
-      message = Message.new(topic: 'topic', text: 'text', from: sender,
-                          to: receiver)
+      message = Message.new(topic: 'topic', text: 'text', sender: sender,
+                          receiver: receiver)
       expect(message.topic).to eq('topic')
     end
     it 'checks if new message has a sender' do
       receiver = User.new(username: 'test', password: 'test')
       sender = User.new(username: 'test', password: 'test')
-      message = Message.new(topic: 'topic', text: 'text', from: sender,
-                          to: receiver)
-      expect(message.from).to eq(sender)
+      message = Message.new(topic: 'topic', text: 'text', sender: sender,
+                          receiver: receiver)
+      expect(message.sender).to eq(sender)
     end
     it 'checks if new message has a receiver' do
       receiver = User.new(username: 'test', password: 'test')
       sender = User.new(username: 'test', password: 'test')
-      message = Message.new(topic: 'topic', text: 'text', from: sender,
-                          to: receiver)
-      expect(message.to).to eq(receiver)
+      message = Message.new(topic: 'topic', text: 'text', sender: sender,
+                          receiver: receiver)
+      expect(message.receiver).to eq(receiver)
     end
     it 'checks if new message has a date when it was sent' do
       receiver = User.new(username: 'test', password: 'test')
       sender = User.new(username: 'test', password: 'test')
-      message = Message.new(topic: 'topic', text: 'text', from: sender,
-                          to: receiver)
+      message = Message.new(topic: 'topic', text: 'text', sender: sender,
+                          receiver: receiver)
       expect(message.date).to be_a(Time)
     end
     it 'checks if new message has a text' do
       receiver = User.new(username: 'test', password: 'test')
       sender = User.new(username: 'test', password: 'test')
-      message = Message.new(topic: 'topic', text: 'text', from: sender,
-                          to: receiver)
+      message = Message.new(topic: 'topic', text: 'text', sender: sender,
+                          receiver: receiver)
       expect(message.text).to eq('text')
     end
   end
