9c53b59be18ce410258f9e8cf7a26a3cb065561b
Gintas1 <gintautas.kras@gmail.com>
2014-11-01 20:07:59 +0200

    users can now register and login

--- lib/shop.rb
+++ lib/shop.rb
@@ -17,10 +17,78 @@ class Shop
   end
   
   def begin
+    puts 'welcome to online store ' + @name
+    loop do
+      puts 'what would you like to do?'
+      puts 'reg - register'
+      puts 'login - to login'
+      puts 'exit - to exit'
+      puts
+      action = gets
+      action.rstrip!
+      case action
+        when 'reg'
+          register
+        when 'login'
+          login
+        when 'exit'
+          break
+        else
+          puts 'wrong action'
+          puts
+      end
+    end
+  end
+  
+  def register
+    loop do
+      puts 'do you want to go back? [y/n]'
+      if gets.rstrip! == 'y'
+        break
+      end
+      puts 'enter username:'
+      username = gets
+      username.rstrip!
+      puts 'enter password'
+      password = gets
+      password.rstrip!
+      if @users.any? { |user| user.check_username(username) }
+        puts 'username is already in use'
+        puts
+      else
+        @users.push(User.new(username: username, password:password))
+        puts 'succes! you have registered'
+        puts
+        break
+      end
+    end
+  end
+  
+  def login
+    loop do
+      puts 'do you want to go back?[y/n]'
+      if gets.rstrip! == 'y'
+        break
+      end
+	  puts 'enter username'
+	  username = gets.rstrip!
+	  puts 'enter password'
+	  password = gets.rstrip!
+      @current_user = @users.find { |user| user.check_data(username, password) }
+	  puts @current_user
+	  if @current_user.is_a?(User) || @current_user.is_a?(Admin)
+        puts 'you have loged in'
+        break
+      else
+        puts 'Wrong username or password'
+      end
+    end
   end
 end
 
-#shop = Shop.new
-#shop.begin
+shop = Shop.new
+#shop.users.push(User.new(username: 'a', password: 'a'))
+shop.begin
+
 #admin = Admin.new(username: 'test', password: 'test')
 #admin.sort
